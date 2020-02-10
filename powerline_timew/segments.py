b67809e2509b0b9ea76b28b1c760e09601cf00f9
Javier Celaya <javier.celaya@sysdig.com>
2020-02-09 21:15:31 -0800

    Change highlight group when limit reached

--- powerline_timew/segments.py
+++ powerline_timew/segments.py
@@ -1,37 +1,37 @@
 # vim:fileencoding=utf-8:noet
 
-import os
 import string
-from subprocess import PIPE, Popen
+from subprocess import PIPE, run
 
 from powerline.segments import Segment, with_docstring
 
 
 class TimewSegment(Segment):
 
-    def execute(self, pl, task):
-        pl.debug('Executing command: timew summary today %s' % task)
+    def summary(self, task):
+        proc = run(['timew', 'summary', 'today', ':id', task], stdout=PIPE)
+        return proc.stdout.decode('UTF-8').splitlines()
 
-        git_env = os.environ.copy()
-        git_env['LC_ALL'] = 'C'
+    def limit_expired(self, limit, time_today):
+        for i, j in zip(limit.split(':'), time_today.split(':')):
+            if int(i) < int(j):
+                return True
+            elif int(i) > int(j):
+                return False
+        return True
 
-        proc = Popen(['timew', 'summary', 'today', task], stdout=PIPE,
-                     stderr=PIPE, env=git_env)
-        out, err = [item.decode('utf-8') for item in proc.communicate()]
-
-        if out:
-            pl.debug('Command output: %s' % out.strip(string.whitespace))
-        if err:
-            pl.debug('Command errors: %s' % err.strip(string.whitespace))
-
-        return out.splitlines()
-
-    def __call__(self, pl, task='work'):
-        pl.debug('Running timew for task %s' % task)
-
-        output = self.execute(pl, task)
+    def __call__(self, pl, task='work', limit='8:00:00'):
+        output = self.summary(task)
         if len(output) > 1:
-            return output[len(output) - 2].strip(string.whitespace)
+            time_today = output[len(output) - 2].strip(string.whitespace)
+            expired = self.limit_expired(limit, time_today)
+            group = 'timew_expired' if expired else 'timew_unexpired'
+            return [
+                {
+                    'contents': time_today,
+                    'highlight_groups': [group, 'timew']
+                }
+            ]
         return []
 
 
